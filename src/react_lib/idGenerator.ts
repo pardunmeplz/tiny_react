56aaf6a3ef3597897e3a27cd4e0c53f277de216c
Parth Dundh <dundh.parth@gmail.com>
2025-12-01 01:12:03 +0530

    id generation bugfix

--- src/react_lib/idGenerator.ts
+++ src/react_lib/idGenerator.ts
@@ -1,7 +1,7 @@
 const idGenerator = {
     id: [0],
     getId: function() { return this.id.reduce((a, b) => a + "." + b, "") },
-    replace: function(id: number) { this.id[this.id.length] = id },
+    replace: function(id: number) { this.id[this.id.length - 1] = id },
     addChild: function() { this.id.push(0) },
     dropChild: function() { this.id.pop() },
     reset: function() { this.id = [0] }
